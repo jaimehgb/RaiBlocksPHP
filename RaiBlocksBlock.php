8347a3da034e0357ff14272791ac5b5a2d22bf4e
jaimehgb <jaimehgb@users.noreply.github.com>
2017-10-10 18:34:47 +0200

    Added class to build RaiBlocks blocks

---
+++ RaiBlocksBlock.php
@@ -0,0 +1,182 @@
+<?php
+require_once 'RaiBlocks.php';
+
+class RaiBlocksBlock extends RaiBlocks
+{
+	
+	private $type;
+	private $hash;
+	private $previous;
+	private $balance;
+	private $destination;
+	
+	private $source;
+	
+	public function build()
+	{
+		$hash = '';
+		switch($this->type)
+		{
+			case 'send':
+				$output = new SplFixedArray(64);
+				$blake2b = new Blake2b();
+				$ctx = $blake2b->init(null, 32);
+				$blake2b->update($ctx, $this->previous->toUint8(), 32);
+				$blake2b->update($ctx, $this->destination->toUint8(), 32);
+				$blake2b->update($ctx, $this->balance->toUint8(), 16);
+				$blake2b->finish($ctx, $output);
+				
+				$hash = new SplFixedArray(32);
+				for($i = 0; $i < 32; $i++)
+					$hash[$i] = $output[$i];
+				$this->hash = (new Uint())->fromUint8Array($hash);
+				break;
+			
+			case 'receive':
+				$output = new SplFixedArray(64);
+				$blake2b = new Blake2b();
+				$ctx = $blake2b->init(null, 32);
+				$blake2b->update($ctx, $this->previous->toUint8(), 32);
+				$blake2b->update($ctx, $this->source->toUint8(), 32);
+				$blake2b->finish($ctx, $output);
+				
+				$hash = new SplFixedArray(32);
+				for($i = 0; $i < 32; $i++)
+					$hash[$i] = $output[$i];
+				$this->hash = (new Uint())->fromUint8Array($hash);
+				break;
+			
+			case 'open':
+				$output = new SplFixedArray(64);
+				$blake2b = new Blake2b();
+				$ctx = $blake2b->init(null, 32);
+				$blake2b->update($ctx, $this->source->toUint8(), 32);
+				$blake2b->update($ctx, $this->representative->toUint8(), 32);
+				$blake2b->update($ctx, $this->account->toUint8(), 32);
+				$blake2b->finish($ctx, $output);
+				
+				$hash = new SplFixedArray(32);
+				for($i = 0; $i < 32; $i++)
+					$hash[$i] = $output[$i];
+				$this->hash = (new Uint())->fromUint8Array($hash);
+				break;
+			
+			case 'change':
+				$output = new SplFixedArray(64);
+				$blake2b = new Blake2b();
+				$ctx = $blake2b->init(null, 32);
+				$blake2b->update($ctx, $this->previous->toUint8(), 32);
+				$blake2b->update($ctx, $this->representative->toUint8(), 32);
+				$blake2b->finish($ctx, $output);
+				
+				$hash = new SplFixedArray(32);
+				for($i = 0; $i < 32; $i++)
+					$hash[$i] = $output[$i];
+				$this->hash = (new Uint())->fromUint8Array($hash);
+				break;
+			
+			default:
+				throw new InvalidBlockTypeException('Block type should be open/send/receive/change: ' . $type);
+		}
+		return $this->hash;
+	}
+	
+	public function send($previous, $destination, $balance)
+	{
+		if(!preg_match('/[0-9A-F]{64}/i', $previous))
+			throw new InvalidBlockHashException('Previous block hash is not valid.');
+		$pk = self::keyFromAccount($destination);
+		if($pk === false)
+			throw new InvalidRaiBlocksAccountException('Destination account is not valid');
+		if(!is_numeric($balance) || $balance < 0)
+			throw new InvalidBalanceException('Balance should be an integer greater than 0');
+		
+		$this->previous = (new Uint())->fromHex($previous);
+		$this->destination = (new Uint())->fromHex($pk);
+		$this->balance = (new Uint())->fromDec($balance);
+		$this->type = 'send';
+	}
+	
+	public function receive($previous, $source)
+	{
+		if(!preg_match('/[0-9A-F]{64}/i', $previous))
+			throw new InvalidBlockHashException('Previous block hash is not valid.');
+		if(!preg_match('/[0-9A-F]{64}/i', $source))
+			throw new InvalidBlockHashException('Source block hash is not valid.');
+		
+		$this->previous = (new Uint())->fromHex($previous);
+		$this->source = (new Uint())->fromHex($source);
+		$this->type = 'receive';
+	}
+	
+	public function open($source, $account, $representative)
+	{
+		if(!preg_match('/[0-9A-F]{64}/i', $source))
+			throw new InvalidBlockHashException('Source block hash is not valid.');
+		
+		$a_pk = self::keyFromAccount($account);
+		$r_pk = self::keyFromAccount($representative);
+		if($a_pk === false)
+			throw new InvalidRaiBlocksAccountException('Open account is not valid');
+		if($r_pk === false)
+			throw new InvalidRaiBlocksAccountException('Representative account is not valid');
+			
+		
+		$this->source = (new Uint())->fromHex($source);
+		$this->representative = (new Uint())->fromHex($r_pk);
+		$this->account = (new Uint())->fromHex($a_pk);
+		$this->type = 'open';
+		
+	}
+	
+	public function change($previous, $representative)
+	{
+		 if(!preg_match('/[0-9A-F]{64}/i', $previous))
+			throw new InvalidBlockHashException('Previous block hash is not valid.');
+			
+		$pk = self::keyFromAccount($representative);
+		
+		if($pk === false)		
+			throw new InvalidRaiBlocksAccountException('Representative account is not valid');
+		$this->previous = (new Uint())->fromHex($previous);
+		$this->representative = (new Uint())->fromHex($pk);
+		$this->type = "change";
+	}
+	
+	public static function checkWork($hash, $work)
+	{
+		if(!hex2bin($work))
+			return false;
+		if(strlen($work) != 16)
+			return false;
+		if(strlen($hash) != 64)
+			return false;
+		if(!hex2bin($hash))
+			return false;
+		
+		$res = new SplFixedArray(64);
+		$workBytes = Uint::fromHex($work)->toUint8();
+		$hashBytes = Uint::fromHex($hash)->toUint8();
+		$workBytes = array_reverse($workBytes->toArray());
+		$workBytes = SplFixedArray::fromArray($workBytes);
+		
+		$blake2b = new Blake2b();
+		$ctx = $blake2b->init(null, 8);
+		$blake2b->update($ctx, $workBytes, 8);
+		$blake2b->update($ctx, $hashBytes, 32);
+		$blake2b->finish($ctx, $res);
+		
+		if($res[7] == 255)
+			if($res[6] == 255)
+				if($res[5] == 255)
+					if($res[4] >= 192)
+						return true;
+		return false;
+	}
+	
+	public function getHash()
+	{
+		return $this->hash;
+	}
+}
+
---
+++ Tests/blocks.php
@@ -0,0 +1,39 @@
+<?php
+
+
+//require '../RaiBlocksBlock.php';
+require '../autoload.php';
+
+$block = new RaiBlocksBlock();
+
+$block->send(
+	$previous='1967EA3553D6CEA90987D6C2CD5F101278823F497DA5527A6E1C0F2F3E5BF801',
+	$destination='xrb_3wm37qz19zhei7nzscjcopbrbnnachs4p1gnwo5oroi3qonw6inwgoeuufdp',
+	$balance="1383752549851325167221744836530526348"
+);
+
+$block->build();
+echo $block->getHash()->toHexString(); // F927A61F6CB97A54166A6532C3BCCCF3D184C7B62301D7396470E6ACE18C4248
+echo "\n";
+
+$block->receive('DC04354B1C20C0DC0D14339B50F428B95481DFE627507FE64862BAE8FA2661B2', 'DC1E2B3F7C6BBE67990EF8F31E288F47EDA9B82893CA869E25955182A0E26B4A');
+$block->build();
+echo $block->getHash()->toHexString(); // 7D3844CD04DF96022113D0E1C4785D565CEF2C9629A96868C3C9876C90BFF92A
+echo "\n";
+
+$block->open(
+	$source='BE548A457F28A42119FB22373303690DAB5F40811E4E87643A15267827D6C138',
+	$account='xrb_3pg6xswkroybxydyzaxybb1h531sx34omiu7an9t9jy19f9mca7a36s7by5e',
+	$representative='xrb_1anrzcuwe64rwxzcco8dkhpyxpi8kd7zsjc1oeimpc3ppca4mrjtwnqposrs'
+);
+$block->build();
+echo $block->getHash()->toHexString(); // 361B2201C899AF56D3901CCEF11B1CBD728CD5563721124F3E944AED277D2425	
+echo "\n";
+
+$block->change(
+	$previous='ECCF08BBB0556D8527CC739C01DDC466415300B781B5988E8FE9A62D765FDD5C',
+	$representative='xrb_1nkquu5dnehkn7fw69ksqh37fkrjc7oao4rrc3f37m868tohxizywzagjc89'
+);
+$block->build();
+echo $block->getHash()->toHexString(); // FF14307F67B85C8F4EE903447E24818618CEA7B9C45DCCF6819F8F129CE63DB1	
+echo "\n";
