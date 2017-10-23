90ae3c4509d838d79ebf66357e6e218c61e5efd1
jaimehgb <jaimehgb@users.noreply.github.com>
2017-10-23 15:06:46 +0200

    Added new exception types

--- RaiBlocksExceptions.php
+++ RaiBlocksExceptions.php
@@ -1,6 +1,9 @@
 <?php
 
 class InvalidBlockTypeException extends Exception{}
+class IncompleteBlockException extends Exception{}
+class InsufficientWorkException extends Exception{}
 class InvalidRaiBlocksAccountException extends Exception{}
+class InvalidRaiBlocksKeyException extends Exception{}
 class InvalidBlockHashException extends Exception{}
 class InvalidBalanceException extends Exception{}
